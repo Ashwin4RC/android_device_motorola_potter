7adfb9fa2a34a11171cd73fef6f874021696247b
Alberto97 <albertop2197@gmail.com>
2018-08-11 21:53:23 +0200

    Add Camera shims

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -230,7 +230,9 @@ MOT_SENSOR_HUB_FEATURE_GR := true
 # Shim
 TARGET_LD_SHIM_LIBS := \
     /system/vendor/bin/adspd|libshim_adsp.so \
-    /system/vendor/lib64/libmdmcutback.so|libqsap_shim.so
+    /system/vendor/lib64/libmdmcutback.so|libqsap_shim.so \
+    /system/vendor/lib/libmot_gpu_mapper.so|libgpu_mapper_shim.so \
+    /system/lib/libjustshoot.so|libjustshoot_shim.so
 
 # Thermal
 USE_DEVICE_SPECIFIC_THERMAL := true
